0f1f2c37d86894f7baab3204662c8e0f006d806d
Diana Sosnovskaya <progdidi@gmail.com>
2023-10-03 17:30:03 +0200

    modal

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,31 @@ $(function(){
     headerBtn.addEventListener('mouseout', () => {
         headerBtn.classList.remove('green-btn');
     })
+
+    //modal
+
+    const buttons = document.querySelectorAll('button');
+    const modal = document.querySelector('.modal');
+    const closeBtn = document.querySelector('.close-btn');
+    buttons.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('open');
+        });
+    })
+    closeBtn.addEventListener('click', () => {
+        modal.classList.remove('open');
+    })
+
+    modal.addEventListener('click', (e) => {
+        if(!e.target.classList.contains('modal__content')) {
+            modal.classList.remove('open');
+        }
+    })
+
+    document.addEventListener('keydown', (e) => {
+        if(e.key === 'Escape') {
+            modal.classList.remove('open');
+        }
+    })
    
 });
